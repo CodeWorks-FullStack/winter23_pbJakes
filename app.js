92848ea98717a0612e25fde3bd667e153bbf86e9
Jake Overall <joverall22@gmail.com>
2023-01-25 11:01:42 -0700

    woooooo! I should have commited before this

---
+++ app.js
@@ -0,0 +1,45 @@
+let store = [{
+  name: "nutella",
+  img: 'https://images.unsplash.com/photo-1603701972178-96760b471be9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80',
+  price: 22.93,
+  inStock: true,
+  sku: '12j'
+}]
+
+let cart = []
+
+
+// SECTION DRAW FUNCTIONS
+//#region - Draw Functions 
+
+function drawStore(){
+
+}
+
+function drawCart(){
+
+}
+
+//#endregion
+
+function addItemToCart(sku){
+
+}
+
+
+
+function calculateCartTotal(){
+
+}
+
+function checkout(){
+
+}
+
+function removeItem(sku){
+
+}
+
+// TODO stretch goal
+// function adjustQuantity(){}
+
