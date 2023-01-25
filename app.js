5737d807f424e20a2fed42e1bb4b8d0b80c7d95a
Jake Overall <joverall22@gmail.com>
2023-01-25 14:54:17 -0700

    student handoff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let store = [{
   price: 22.93,
   inStock: true,
   sku: '12j'
-},{
+}, {
   name: '💖 Stopper',
   img: 'https://images.unsplash.com/photo-1625932948487-10d58571c487?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
   price: 19.99,
@@ -12,28 +12,13 @@ let store = [{
   sku: '7744hs'
 }]
 
-let cart = [
-  // {
-  //   name: 'Nutella',
-  //   img: 'https://images.unsplash.com/photo-1603701972178-96760b471be9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80',
-  //   price: 22.93,
-  //   sku: '12j',
-  //   quantity: 1
-  // },
-  // {
-  //   name: '🤖',
-  //   img: 'https://images.unsplash.com/photo-1603701972178-96760b471be9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2864&q=80',
-  //   price: 22000.93,
-  //   sku: '12jj',
-  //   quantity: 1
-  // }
-]
+let cart = []
 
 
 // SECTION DRAW FUNCTIONS
 //#region - Draw Functions 
 
-function drawStore(){
+function drawStore() {
 
   let storeElem = document.getElementById('product-cards')
   let template = ''
@@ -62,56 +47,94 @@ function drawStore(){
     </div>`
   }
 
-  
-  
+
+
   // store.forEach(product => {
-    // you could paste template += here 
+  // you could paste template += here 
   // })
-    
+
   storeElem.innerHTML = template
-  
+
 
 
 
   console.log('drawStore')
 }
 
-function drawCart(){
+function drawCart() {
   // console.log('drawCart')
 
   let cartElem = document.getElementById('cart')
+  let cartTotalElem = document.getElementById('cart-total')
   let template = ''
 
-  cart.forEach(item =>{
-    template += `<div>${item.name}</div>`
+  cart.forEach(item => {
+    template += `<div>${item.name} - ${item.quantity} 
+    <button 
+      onclick="removeItem('${item.sku}')" 
+      class="btn btn-danger"> <i class="mdi mdi-delete-forever"></i> </button></div>`
   })
 
-
+  let total = calculateCartTotal()
 
   cartElem.innerHTML = template
+  cartTotalElem.innerText = total.toFixed(2)
 
 }
 
 //#endregion
 
-function addItemToCart(sku){
-  // console.log('addItemToCart', sku)
+function addItemToCart(sku) {
+
+  let itemToAdd = store.find(p => p.sku == sku)
+  console.log(itemToAdd)
+
+  let productFoundInCart = cart.find(p => p.sku == sku)
+
+  // TRUTHY: https://developer.mozilla.org/en-US/docs/Glossary/Truthy
+  // FALSY: https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+  if (productFoundInCart) {
+    productFoundInCart.quantity++
+  } else {
+    cart.push({
+      name: itemToAdd.name,
+      price: itemToAdd.price,
+      sku: itemToAdd.sku,
+      img: itemToAdd.img,
+      quantity: 1
+    })
+  }
+
+  // ✅ figure which item you clicked on?
+  // ✅ if that item is in the cart up the quantity
+  // ✅ otherwise add it to the cart
 
   drawCart()
 
 }
 
 
-function calculateCartTotal(){
-  console.log('calculateCartTotal')
+function calculateCartTotal() {
+
+  let total = 0
+  cart.forEach(p => {
+    total += p.price * p.quantity
+  })
+
+  return total
 }
 
-function checkout(){
-  console.log('checkout')
+function checkout() {
+  cart = []
+  drawCart()
 }
 
-function removeItem(sku){
- console.log('removeItem')
+function removeItem(sku) {
+  
+  let index = cart.findIndex(p => p.sku == sku)
+  cart.splice(index, 1)
+  drawCart()
+  
 }
 
 // TODO stretch goal
